992136e178b5740a286853f056b066f4c6b44bb8
blindyw <blindyw@blindywui-iMac.local>
2018-10-08 14:52:41 +0900

    sendbird-await bug fix.

--- SendbirdAwait.js
+++ SendbirdAwait.js
@@ -7,12 +7,12 @@ const Message = require('./lib/Message');
 const Metadata = require('./lib/Metadata');
 
 class SendbirdAwait {
-    constructor(apiToken) {
-        this.user =         new User(apiToken);
-        this.message =      new Message(apiToken);
-        this.groupchannel = new GroupChannel(apiToken);
-        this.openchannel =  new OpenChannel(apiToken);
-        this.metadata =     new Metadata(apiToken);
+    constructor(apiToken, regionIdentifier = 'api', version = 'v3') {
+        this.user =         new User(apiToken, regionIdentifier, version);
+        this.message =      new Message(apiToken, regionIdentifier, version);
+        this.groupchannel = new GroupChannel(apiToken, regionIdentifier, version);
+        this.openchannel =  new OpenChannel(apiToken, regionIdentifier, version);
+        this.metadata =     new Metadata(apiToken, regionIdentifier, version);
     }
 }
 
